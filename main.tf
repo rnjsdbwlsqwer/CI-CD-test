30b7ee6d476c89a10d1caeab1f5f9d1178cbc9fa
rnjsdbwlsqwer <rnjsdbwls2006@naver.com>
2025-05-28 22:48:00 +0900

    .

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_iam_role" "github_oidc_role" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = "repo:rnjsdbwlsqwer/CI-CD-test:*"
+            "token.actions.githubusercontent.com:sub" = "repo:rnjsdbwlsqwer/CI-CD-test:ref:refs/heads/main"
           }
         }
       }
